a035b86ea061b9590e5972e471152395bd212d5c
Rizki Gunawan <rizkigunawan9901@gmail.com>
2024-05-03 01:39:09 +0700

    feat: slicing detail movie

--- resources/js/Components/FeaturedMovie.jsx
+++ resources/js/Components/FeaturedMovie.jsx
@@ -50,7 +50,10 @@ export default function FeaturedMovie({
                     <img src="/icons/ic_play.svg" width="50" alt="" />
                 </div>
             </div>
-            <a href={slug} className="inset-0 absolute z-50"></a>
+            <Link
+                href={route("prototype.movie.show", slug)}
+                className="inset-0 absolute z-50"
+            ></Link>
         </div>
     );
 }
