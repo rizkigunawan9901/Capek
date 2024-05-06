1bb67a9017b0bfb0cf70b4cd512448b1005247aa
Rizki Gunawan <rizkigunawan9901@gmail.com>
2024-05-06 23:20:59 +0700

    feat: init integration dashboard

--- database/seeders/MovieTableSeeder.php
+++ database/seeders/MovieTableSeeder.php
@@ -18,20 +18,29 @@ class MovieTableSeeder extends Seeder
         $movies = [
             [
                 'name' => 'The Shawshank Redemption',
-                'slug' => 'the-shawshanks-redemption',
+                'slug' => 'the-shawshank-redemption',
                 'category' => 'Drama',
-                'video_url' => 'https://youtu.be/YJTKlAvbDo4',
-                'thumbnail' => 'https://fastly.picsum.photos/id/1/300/300.jpg',
-                'rating' => 9.3,
+                'video_url' => 'https://www.youtube.com/watch?v=6hB3S9bIaco',
+                'thumbnail' => 'https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg',
+                'rating' => 4.3,
                 'is_featured' => 1,
             ],
             [
-                'name' => 'The Shank Redemption',
-                'slug' => 'the-shanks-redemption',
+                'name' => 'The Godfather',
+                'slug' => 'the-godfather',
                 'category' => 'Drama',
-                'video_url' => 'https://youtu.be/YJTKlAvbDo4',
-                'thumbnail' => 'https://fastly.picsum.photos/id/1/300/300.jpg',
-                'rating' => 9.3,
+                'video_url' => 'https://www.youtube.com/watch?v=sY1S34973zA',
+                'thumbnail' => 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,704,1000_AL_.jpg',
+                'rating' => 4.2,
+                'is_featured' => 0,
+            ],
+            [
+                'name' => 'The Godfather: Part II',
+                'slug' => 'the-godfather-part-ii',
+                'category' => 'Drama',
+                'video_url' => 'https://www.youtube.com/watch?v=sY1S34973zA',
+                'thumbnail' => 'https://m.media-amazon.com/images/M/MV5BMWMwMGQzZTItY2JlNC00OWZiLWIyMDctNDk2ZDQ2YjRjMWQ0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,704,1000_AL_.jpg',
+                'rating' => 4.0,
                 'is_featured' => 0,
             ]
             ];
